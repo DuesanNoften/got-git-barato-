17756838cd9286929ea97cb7ebf5a56912cff86c
Josue3108 <67967986+Josue3108@users.noreply.github.com>
2022-11-26 15:00:21 -0300

    Servidor inicio
    Se creo el server con un API REST

---
+++ Server/Controllers/Help.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Help : ControllerBase
+    {
+        [HttpGet("Got/{id}")]
+        public string Get( string id)
+        {
+            //id switch{
+                //case
+            //}
+            string i = "a";
+            return i;
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public class Startup
+{
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddControllers();
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		if(env.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
+
+		app.UseRouting();
+
+		app.UseEndpoints(endpoints =>
+		{
+			endpoints.MapGet("/", async context =>
+			{
+				await context.Response.WriteAsync("Hello World");
+			});
+			endpoints.MapControllers();
+
+		});
+	}
+	public Startup()
+	{
+	}
+}
